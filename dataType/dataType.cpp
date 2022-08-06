a0963ab9a8306171f195fc16dca8c80ada9a2661
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-08-06 10:41:57 +0600

    data type (int, float, double, boolean, char, string) example

---
+++ dataType/dataType.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //integer
+    int num=10;
+    cout<<"Integer DataType:"   <<num   <<endl;
+
+    //float
+    float res=10.55;
+    cout<<"Float DataType:"   <<res   <<endl;
+
+    //double
+    double data=10.554455;
+    cout<<"Double DataType:"   <<data   <<endl;
+
+    //boolean
+    bool isTrue=true;
+    cout<<"Boolean DataType:"   <<isTrue   <<endl;
+
+    //char
+    char ch='A';
+    cout<<"Char DataType:"   <<ch   <<endl;
+
+    //string
+    string str="Hello world";
+    cout<<"String DataType:"   <<str   <<endl;
+
+    return 0;
+}
