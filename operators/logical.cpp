29508e8bd599cd53bd60c18d6a122f2086ef4e56
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-08-08 09:54:57 +0600

    logical operator example added

---
+++ operators/logical.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int value1=10, value2=20;
+
+    if(value1>0 && value1<value2 ){
+        cout<<"value1 is less then value2" <<endl;
+    }
+    return 0;
+}
