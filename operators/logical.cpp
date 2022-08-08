728998c9dc67a642c7b74df7e21abc1be3a4eef1
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-08-08 10:07:16 +0600

    logical operator (&&, ||, ! ) example

--- operators/logical.cpp
+++ operators/logical.cpp
@@ -5,8 +5,24 @@ int main(){
 
     int value1=10, value2=20;
 
+    //logical and (&&) operator
     if(value1>0 && value1<value2 ){
         cout<<"value1 is less then value2" <<endl;
     }
+
+    //logical or
+    if(value1>0 || value1<value2 ){
+        cout<<"value1 0 to " <<value2 <<endl;
+    }
+
+    //logical not
+    int a=5, b=5;
+    if(a != b){
+        cout<<"allow" <<endl;
+    }
+    else{
+        cout<<"not allow" <<endl;
+    }
+
     return 0;
 }
