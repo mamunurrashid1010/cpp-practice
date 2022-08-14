0b58289e6118e78f08eee861ea19cc3c1d30afaf
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-08-14 09:31:18 +0600

    C++ mathematical operation, get max and min using buildin function

---
+++ math/math.cpp
@@ -0,0 +1,26 @@
+/*
+    C++
+    Math
+    c++ have many function to perform different mathematical operation
+    functions: max(),min(),pow(),sqrt(),round(),log(),abs() etc.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //max and min
+    int v1,v2;
+    cout<<"Enter 2 value for calculate max and min:";
+    cin>>v1;
+    cin>>v2;
+    cout<<"Max is: " <<max(v1,v2) <<endl;
+    cout<<"Min is: " <<min(v1,v2) <<endl;
+
+    return 0;
+
+}
+
+
+
