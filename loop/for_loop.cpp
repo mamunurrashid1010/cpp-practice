df54f284d253db9129360ea7782ce39b22a7532a
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-08-18 09:27:44 +0600

    for loop example, print 1 to 10 and print odd number example added

---
+++ loop/for_loop.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //example-1
+    cout<<"Print 1 to 10:" <<endl;
+    for(int i=1;i<=10;i++){
+        cout<<i <<endl;
+    }
+    cout<<endl;
+
+    //example-1
+    cout<<"Print odd number from 1 to 10:" <<endl;
+    for(int i=1;i<=10;i+=2){
+        cout<<i <<endl;
+    }
+
+    return 0;
+}
