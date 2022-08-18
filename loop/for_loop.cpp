10658afa8c208519d9b25c6ca642694f451d057a
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-08-18 09:53:08 +0600

    for loop, print even number and pattern print program

--- loop/for_loop.cpp
+++ loop/for_loop.cpp
@@ -10,10 +10,34 @@ int main(){
     }
     cout<<endl;
 
-    //example-1
+    //example-2
     cout<<"Print odd number from 1 to 10:" <<endl;
     for(int i=1;i<=10;i+=2){
         cout<<i <<endl;
+    }
+
+    //example-3
+    cout<<"Print Even number from 1 to 10:" <<endl;
+    for(int i=2;i<=10;i+=2){
+        cout<<i <<endl;
+    }
+
+    //example-4
+    cout<<"Print Pettern :" <<endl;
+    for(int i=1;i<=10;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    //example-5
+    cout<<"Print pattern square :" <<endl;
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5;j++){
+            cout<<"*";
+        }
+        cout<<endl;
     }
 
     return 0;
