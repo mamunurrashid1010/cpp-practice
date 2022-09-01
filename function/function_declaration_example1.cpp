e94c61ccce859c41deb94dba2d35427c67450b37
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-09-01 10:00:22 +0600

    Function: function declaration and function calling

---
+++ function/function_declaration_example1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+// function 1 create
+void myFunction1(){
+    cout<<"I am from myFunction1" <<endl;
+}
+
+// function 2 create
+void myFunction2(){
+    cout<<"I am from myFunction2" <<endl;
+}
+
+// function 3 create
+void myFunction3(){
+    cout<<"I am from myFunction3" <<endl;
+}
+
+int main(){
+
+    // function calling
+    myFunction1();
+    myFunction2();
+    myFunction3();
+
+    return 0;
+}
