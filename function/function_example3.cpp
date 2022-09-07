9d13614bcf8bd5b3b9cc363700530547d9065ef0
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-09-07 09:20:33 +0600

    Function: pass array as function parameter example

--- function/function_example3.cpp
+++ function/function_example3.cpp
@@ -8,6 +8,15 @@ void employeeInfo(string name, int age, string designation){
     cout<<"Designation: " <<designation <<endl;
     return;
 }
+
+// pass array as function parameter
+int sum(int myArr[5]){
+    int sum=0;
+    for(int i=0;i<5;i++){
+        sum+=myArr[i];
+    }
+    return sum;
+}
 int main(){
 
     // function call
@@ -17,6 +26,12 @@ int main(){
     employeeInfo("Abrahim",25,"Web Designer");
     cout<<endl;
     employeeInfo("Smith",32,"Project Manager");
+
+    // array declaration
+    int myArr[5]={10,11,12,13,14};
+    int totalSum=sum(myArr);
+    cout<<endl;
+    cout<<"The sum is:" <<totalSum <<endl;
 
     return 0;
 }
